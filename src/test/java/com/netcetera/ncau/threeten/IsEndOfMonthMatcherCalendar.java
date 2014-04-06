6ed395df5c7fb5ec5c29cac104af4ed4e8499e4f
Philippe Marschall <philippe.marschall@gmail.com>
2014-04-06 19:07:51 +0200

    Explicitly set time zone
    This should fix the build.

--- src/test/java/com/netcetera/ncau/threeten/IsEndOfMonthMatcherCalendar.java
+++ src/test/java/com/netcetera/ncau/threeten/IsEndOfMonthMatcherCalendar.java
@@ -5,17 +5,18 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 import org.hamcrest.Description;
 import org.hamcrest.Factory;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 
-import com.netcetera.ncau.threeten.HaggiChecker;
-
 final class IsEndOfMonthMatcherCalendar extends TypeSafeMatcher<String> {
 
+  private static final String ZONE_ID = "CET";
   private static final IsEndOfMonthMatcherCalendar INSTANCE = new IsEndOfMonthMatcherCalendar();
+  private static final TimeZone ZONE = TimeZone.getTimeZone(ZONE_ID);
 
   @Override
   public void describeTo(Description description) {
@@ -26,8 +27,8 @@ final class IsEndOfMonthMatcherCalendar extends TypeSafeMatcher<String> {
   protected boolean matchesSafely(String dateString) {
     try {
       SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz");
-      Date date = dateFormat.parse(dateString + "CET");
-      Calendar calendar = GregorianCalendar.getInstance();
+      Date date = dateFormat.parse(dateString + ZONE_ID);
+      Calendar calendar = GregorianCalendar.getInstance(ZONE);
       calendar.setTime(date);
       return HaggiChecker.isEndOfMonth(calendar);
     } catch (ParseException e) {
